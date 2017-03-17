917100cd885fb3a2e335d0785e0d5d8273f388f0
Alexandra Roth <alroth8@gmail.com>
2017-03-17 12:56:15 -0400

    Done.

--- index.js
+++ index.js
@@ -3,11 +3,26 @@ var cart;
 function setCart(newCart) {
   cart = newCart;
 }
+const getCart = () => cart
+
+const viewCart = () => {
+	if(!cart[0]){
+		console.log("Your shopping cart is empty.")
+		return	
+	}
+
+	let itemsSummary = cart.map((item) => {
+		let itemName = Object.keys(item)[0]
+		let itemPrice = item[itemName]
+		return `${itemName} at $${itemPrice}`
+	})
+	console.log(`In your cart, you have ${itemsSummary.join(", ")}.`)
+}
 
 function total() {
   let t = 0
 
-  for (var i = 0, l = cart.length; i < l; i++) {
+  for (var i = 0; i < cart.length; i++) {
     for (var item in cart[i]) {
       t += cart[i][item]
     }
@@ -15,3 +30,47 @@ function total() {
 
   return t
 }
+
+
+
+
+
+const addToCart = (item) => {
+	const price = Math.floor(Math.random() * 100)
+	cart.push({[item]: price})
+	console.log(`${item} has been added to your cart.`)
+	return cart
+}
+
+
+
+
+const removeFromCart = (item) =>{
+	let filteredCart = cart.filter((storedItem) => {
+		storedItem[item]
+	})
+
+	if(filteredCart.length === cart.length) {
+		console.log("That item is not in your cart.")
+	} else {
+		cart = filteredCart
+	}
+}
+
+const placeOrder = (creditCard) => {
+	if (!creditCard) {
+		console.log(`We don't have a credit card on file for you to place your order.`)
+	} 	else {
+	console.log(`Your total cost is $${total()}, which will be charged to the card ${creditCard}.`)
+	}
+	cart.length = 0
+}
+
+
+
+
+
+
+
+
+
